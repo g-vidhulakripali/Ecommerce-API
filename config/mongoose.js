f9354661cf4060aee0c5ae3f30cc0185bce67d9c
g-vidhulakripali <g.vidhulakripali@gmail.com>
2023-07-26 17:24:28 +0530

    MongoDb Atlas connection

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/ecommerce-api-db");
-// mongoose.connect(
-//   "mongodb+srv://gvidhulakripali:Oey4aY6N1BI9N57K@cluster0.squt5dz.mongodb.net/ecommerce-api-db"
-// );
+// mongoose.connect("mongodb://127.0.0.1:27017/ecommerce-api-db");
+mongoose.connect(
+  "mongodb+srv://gvidhulakripali:Oey4aY6N1BI9N57K@cluster0.squt5dz.mongodb.net/ecommerce-api-db"
+);
 const db = mongoose.connection;
 db.on("error", console.log.bind(console, "Error connecting to MongoDB"));
 db.once("open", function () {
